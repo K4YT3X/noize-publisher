747c8c744f640b38f3220d00175053174e456797
K4YT3X <k4yt3x@k4yt3x.com>
2018-12-01 13:43:01 -0500

    1.0.0 basic functionalities

---
+++ bin/noize_publisher.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Name: Noize Server
+Author: K4YT3X
+Date Created: December 1, 2018
+Last Modified: December 1, 2018
+
+Licensed under the GNU General Public License Version 3 (GNU GPL v3),
+    available at: https://www.gnu.org/licenses/gpl-3.0.txt
+
+(C) 2018 K4YT3X
+"""
+from noize_gatherer import NoiseGatherer
+import paho.mqtt.client as mqtt
+import sys
+import traceback
+
+
+class NoizePublisher:
+    """ Noize Publisher
+
+    Publishes sound data to mqtt topic on noize server.
+    This data is to be parsed by noize client to display.
+    """
+
+    def __init__(self, broker, topic):
+        """ Receive and store noize publisher arguments
+        """
+        self.broker = broker
+        self.topic = topic
+
+    def init(self):
+        """ Initialize mqtt client and connect to mqtt server
+        """
+        self.client = mqtt.Client()
+        self.client.on_connect = self._on_connect
+        self.client.connect(self.broker, 1883, 60)
+
+    def publish(self, payload):
+        """ Publish a payload to the topic
+        """
+        self.client.publish(self.topic, payload)
+
+    def _on_connect(self, client, userdata, flags, rc):
+        """ Print the connection result code on connect
+        """
+        print('Connected with result code: {}'.format(str(rc)))
+        # client.subscribe(self.topic)
+
+
+def serve():
+    publisher = NoizePublisher('noize.flexio.org', 'noize/{}'.format(sys.argv[1]))
+    publisher.init()
+    gatherer = NoiseGatherer()
+    while True:
+        level = gatherer.get_level()
+        print('[NoizePublisher]: Publishing Level: {}'.format(level), file=sys.stderr)
+        publisher.publish(int(round(gatherer.get_level()), 0))
+
+
+if __name__ == '__main__':
+    try:
+        serve()
+    except Exception:
+        traceback.print_exc()
+
+# For debugging only
+"""
+if __name__ == '__main__':
+    publisher = NoizePublisher('noize.flexio.org', 'noize/{}'.format(sys.argv[1]))
+    publisher.init()
+
+    while True:
+        publisher.publish(input('[NoizePublisher: {}]> '.format(publisher.topic)))
+"""
